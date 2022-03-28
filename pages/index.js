9632e45e3c9ff0899cfa118919eee740a810acd1
Isaac Lyman <isaaclyman@gmail.com>
2022-03-28 14:34:08 -0600

    Commit working tree from last May

--- pages/index.js
+++ pages/index.js
@@ -12,12 +12,36 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        Main
-      </main>
+        <div className={styles.logo}>
+          <Image src="/logo.png" width={500} height={192} layout="intrinsic"></Image>
+        </div>
 
-      <footer className={styles.footer}>
-        Footer
-      </footer>
+        <p>
+          <strong>qg2</strong> generates a one-page guide to help your coworkers and colleagues work more effectively with you.
+          It includes:
+        </p>
+
+        <ul>
+          <li>Your preferred name and pronouns</li>
+          <li>Your job title</li>
+          <li>A few hobbies and interests</li>
+          <li>Any conditions or disabilities you want to disclose</li>
+          <li>Things people might misunderstand about you</li>
+          <li>Your work environment needs</li>
+          <li>How you like to communicate</li>
+          <li>How you like to receive feedback</li>
+          <li>What motivates you</li>
+          <li>What you struggle with</li>
+        </ul>
+
+        <p>
+          It's free to use and there's no signup. At the end of the process, you'll make a PDF or printout.
+        </p>
+
+        <button>
+          Get started
+        </button>
+      </main>
     </div>
   )
 }
